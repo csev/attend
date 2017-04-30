c54f9d406ac59d39e088b6602e6c973c270052f5
Charles Severance <csev@umich.edu>
2017-04-30 12:32:46 -0400

    Update descriptions

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 $REGISTER_LTI2 = array(
-"name" => "Simple Attendance Tool",
+"name" => "Simple Attendance Tool (Original)",
 "FontAwesome" => "fa-server",
-"short_name" => "Attendance Tool",
-"description" => "This is a simple attendance tool that allows the instructor to set a code and the students enter the code."
+"short_name" => "Original Attendance Tool",
+"description" => "This is a simple attendance tool that allows the instructor to set a code and the students enter the code.  It is coded as an 'old school' Tsugi module."
 );
